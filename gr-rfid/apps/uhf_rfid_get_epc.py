df327518831e274a04579f153d8d3a55c341cb4d
diamant-x <7346338+diamant-x@users.noreply.github.com>
2021-02-12 18:43:29 +0100

    GUI Flowgraph for RealTime 1 Inventory length sync

--- gr-rfid/apps/uhf_rfid_get_epc.py
+++ gr-rfid/apps/uhf_rfid_get_epc.py
@@ -19,16 +19,19 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
+from PyQt5 import Qt
 from PyQt5 import Qt, QtCore
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
+from gnuradio import qtgui
 from gnuradio import uhd
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import rfid
+import sip
 import sys
 import threading
 import time
@@ -65,24 +68,26 @@ class uhf_rfid_get_epc(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self.adc_rate = adc_rate = 2e6
         self.tx_gain = tx_gain = 20
+        self.taps_fir_averaging_samples = taps_fir_averaging_samples = [1]*25
+        self.samplerate_rfidblocks = samplerate_rfidblocks = adc_rate/1
         self.rx_gain = rx_gain = 20
         self.print_results = print_results = 0
-        self.num_taps = num_taps = [1]*25
         self.frequency = frequency = 865.7e6
-        self.decim = decim = 5
-        self.dac_rate = dac_rate = 1e6
-        self.ampl = ampl = 0.9
-        self.adc_rate = adc_rate = 100e6/50
+        self.dac_rate = dac_rate = 1*1e6
+        self.ampl = ampl = 0.8
+        self.GUI_samples = GUI_samples = int(0.8*(44+62+16+62+18+62+96+62)*(adc_rate*24/1000000))
 
         ##################################################
         # Blocks
         ##################################################
-        self.rfid_reader = rfid.reader(int(adc_rate/decim),int(dac_rate))
+        self.rfid_reader = rfid.reader(int(samplerate_rfidblocks),int(dac_rate))
         self.uhd_usrp_source = uhd.usrp_source(
         	",".join(('addr=192.168.10.2', "")),
         	uhd.stream_args(
         		cpu_format="fc32",
+        		otw_format='sc16',
         		channels=range(1),
         	),
         )
@@ -103,8 +108,206 @@ class uhf_rfid_get_epc(gr.top_block, Qt.QWidget):
         self.uhd_usrp_sink.set_center_freq(frequency, 0)
         self.uhd_usrp_sink.set_gain(tx_gain, 0)
         self.uhd_usrp_sink.set_antenna('TX/RX', 0)
-        self.rfid_tag_decoder = rfid.tag_decoder(int(adc_rate/decim))
-        self.rfid_gate = rfid.gate(int(adc_rate/decim))
+        self.rfid_tag_decoder = rfid.tag_decoder(int(samplerate_rfidblocks))
+        self.rfid_gate = rfid.gate(int(samplerate_rfidblocks))
+        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
+        	GUI_samples, #size
+        	adc_rate, #samp_rate
+        	'Phase Plot', #name
+        	2 #number of inputs
+        )
+        self.qtgui_time_sink_x_0_0.set_update_time(1)
+        self.qtgui_time_sink_x_0_0.set_y_axis(-180, 180)
+
+        self.qtgui_time_sink_x_0_0.set_y_label('Phase', 'deg')
+
+        self.qtgui_time_sink_x_0_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_NORM, qtgui.TRIG_SLOPE_NEG, 0.1, 0, 1, "")
+        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0.enable_grid(True)
+        self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0_0.enable_stem_plot(False)
+
+        if not True:
+          self.qtgui_time_sink_x_0_0.disable_legend()
+
+        labels = ['Phase', 'Magnitude', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 0, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 0, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 0.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in xrange(2):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_0_win, 1, 0, 1, 2)
+        for r in range(1, 2):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 2):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        	GUI_samples, #size
+        	adc_rate, #samp_rate
+        	'RSSI', #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(1)
+        self.qtgui_time_sink_x_0.set_y_axis(0, 2)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', 'dB')
+
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_NORM, qtgui.TRIG_SLOPE_NEG, 0.1, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_grid(True)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+        if not True:
+          self.qtgui_time_sink_x_0.disable_legend()
+
+        labels = ['Magnitude', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["red", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 0, 0, 1, 2)
+        for r in range(0, 1):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 2):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_histogram_sink_x_0_0 = qtgui.histogram_sink_f(
+        	GUI_samples,
+        	360,
+                -180,
+                180,
+        	'Phase (deg) Histogram',
+        	1
+        )
+
+        self.qtgui_histogram_sink_x_0_0.set_update_time(1)
+        self.qtgui_histogram_sink_x_0_0.enable_autoscale(True)
+        self.qtgui_histogram_sink_x_0_0.enable_accumulate(False)
+        self.qtgui_histogram_sink_x_0_0.enable_grid(False)
+        self.qtgui_histogram_sink_x_0_0.enable_axis_labels(True)
+
+        if not True:
+          self.qtgui_histogram_sink_x_0_0.disable_legend()
+
+        labels = ['Phase', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_histogram_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_histogram_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_histogram_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_histogram_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_histogram_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_histogram_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_histogram_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_histogram_sink_x_0_0_win = sip.wrapinstance(self.qtgui_histogram_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_histogram_sink_x_0_0_win, 1, 2, 1, 1)
+        for r in range(1, 2):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(2, 3):
+            self.top_grid_layout.setColumnStretch(c, 1)
+        self.qtgui_histogram_sink_x_0 = qtgui.histogram_sink_f(
+        	GUI_samples,
+        	100,
+                -100,
+                1,
+        	'RSSI (dBm?) Histogram Complex to Mag2 to log10',
+        	1
+        )
+
+        self.qtgui_histogram_sink_x_0.set_update_time(1)
+        self.qtgui_histogram_sink_x_0.enable_autoscale(True)
+        self.qtgui_histogram_sink_x_0.enable_accumulate(False)
+        self.qtgui_histogram_sink_x_0.enable_grid(False)
+        self.qtgui_histogram_sink_x_0.enable_axis_labels(True)
+
+        if not True:
+          self.qtgui_histogram_sink_x_0.disable_legend()
+
+        labels = ['Magnitude', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["red", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_histogram_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_histogram_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_histogram_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_histogram_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_histogram_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_histogram_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_histogram_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_histogram_sink_x_0_win = sip.wrapinstance(self.qtgui_histogram_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_histogram_sink_x_0_win, 0, 2, 1, 1)
+        for r in range(0, 1):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(2, 3):
+            self.top_grid_layout.setColumnStretch(c, 1)
 
         def _print_results_probe():
             while True:
@@ -119,42 +322,52 @@ class uhf_rfid_get_epc(gr.top_block, Qt.QWidget):
         _print_results_thread.start()
 
         self.multiply_const_ff = blocks.multiply_const_ff(ampl)
-        self.matched_filter = filter.fir_filter_ccc(decim, (num_taps))
-        self.matched_filter.declare_sample_delay(0)
+        self.fir_filter = filter.fir_filter_ccc(1, (taps_fir_averaging_samples))
+        self.fir_filter.declare_sample_delay(0)
         self.file_sink_source = blocks.file_sink(gr.sizeof_gr_complex*1, '../misc/data/source', False)
         self.file_sink_source.set_unbuffered(False)
-        self.file_sink_reader = blocks.file_sink(gr.sizeof_float*1, '../misc/data/reader', False)
-        self.file_sink_reader.set_unbuffered(False)
-        self.file_sink_matched_filter = blocks.file_sink(gr.sizeof_gr_complex*1, '../misc/data/matched_filter', False)
-        self.file_sink_matched_filter.set_unbuffered(False)
-        self.file_sink_gate = blocks.file_sink(gr.sizeof_gr_complex*1, '../misc/data/gate', False)
-        self.file_sink_gate.set_unbuffered(False)
-        self.file_sink_decoder = blocks.file_sink(gr.sizeof_gr_complex*1, '../misc/data/decoder', False)
-        self.file_sink_decoder.set_unbuffered(False)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((57.295779513082320876798154814105, ))
         self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_complex_to_magphase_0 = blocks.complex_to_magphase(1)
 
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.blocks_complex_to_magphase_0, 1), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_complex_to_magphase_0, 0), (self.qtgui_histogram_sink_x_0, 0))
+        self.connect((self.blocks_complex_to_magphase_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_complex_to_magphase_0, 0), (self.qtgui_time_sink_x_0_0, 1))
         self.connect((self.blocks_float_to_complex_0, 0), (self.uhd_usrp_sink, 0))
-        self.connect((self.matched_filter, 0), (self.file_sink_matched_filter, 0))
-        self.connect((self.matched_filter, 0), (self.rfid_gate, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_histogram_sink_x_0_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_time_sink_x_0_0, 0))
+        self.connect((self.fir_filter, 0), (self.rfid_gate, 0))
         self.connect((self.multiply_const_ff, 0), (self.blocks_float_to_complex_0, 0))
-        self.connect((self.rfid_gate, 0), (self.file_sink_gate, 0))
         self.connect((self.rfid_gate, 0), (self.rfid_tag_decoder, 0))
-        self.connect((self.rfid_reader, 0), (self.file_sink_reader, 0))
         self.connect((self.rfid_reader, 0), (self.multiply_const_ff, 0))
-        self.connect((self.rfid_tag_decoder, 1), (self.file_sink_decoder, 0))
+        self.connect((self.rfid_tag_decoder, 1), (self.blocks_null_sink_0, 0))
         self.connect((self.rfid_tag_decoder, 0), (self.rfid_reader, 0))
+        self.connect((self.uhd_usrp_source, 0), (self.blocks_complex_to_magphase_0, 0))
         self.connect((self.uhd_usrp_source, 0), (self.file_sink_source, 0))
-        self.connect((self.uhd_usrp_source, 0), (self.matched_filter, 0))
+        self.connect((self.uhd_usrp_source, 0), (self.fir_filter, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "uhf_rfid_get_epc")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
+
+    def get_adc_rate(self):
+        return self.adc_rate
+
+    def set_adc_rate(self, adc_rate):
+        self.adc_rate = adc_rate
+        self.set_samplerate_rfidblocks(self.adc_rate/1)
+        self.set_GUI_samples(int(0.8*(44+62+16+62+18+62+96+62)*(self.adc_rate*24/1000000)))
+        self.uhd_usrp_source.set_samp_rate(self.adc_rate)
+        self.qtgui_time_sink_x_0_0.set_samp_rate(self.adc_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.adc_rate)
 
     def get_tx_gain(self):
         return self.tx_gain
@@ -163,6 +376,19 @@ class uhf_rfid_get_epc(gr.top_block, Qt.QWidget):
         self.tx_gain = tx_gain
         self.uhd_usrp_sink.set_gain(self.tx_gain, 0)
 
+
+    def get_taps_fir_averaging_samples(self):
+        return self.taps_fir_averaging_samples
+
+    def set_taps_fir_averaging_samples(self, taps_fir_averaging_samples):
+        self.taps_fir_averaging_samples = taps_fir_averaging_samples
+        self.fir_filter.set_taps((self.taps_fir_averaging_samples))
+
+    def get_samplerate_rfidblocks(self):
+        return self.samplerate_rfidblocks
+
+    def set_samplerate_rfidblocks(self, samplerate_rfidblocks):
+        self.samplerate_rfidblocks = samplerate_rfidblocks
 
     def get_rx_gain(self):
         return self.rx_gain
@@ -178,13 +404,6 @@ class uhf_rfid_get_epc(gr.top_block, Qt.QWidget):
     def set_print_results(self, print_results):
         self.print_results = print_results
 
-    def get_num_taps(self):
-        return self.num_taps
-
-    def set_num_taps(self, num_taps):
-        self.num_taps = num_taps
-        self.matched_filter.set_taps((self.num_taps))
-
     def get_frequency(self):
         return self.frequency
 
@@ -192,12 +411,6 @@ class uhf_rfid_get_epc(gr.top_block, Qt.QWidget):
         self.frequency = frequency
         self.uhd_usrp_source.set_center_freq(self.frequency, 0)
         self.uhd_usrp_sink.set_center_freq(self.frequency, 0)
-
-    def get_decim(self):
-        return self.decim
-
-    def set_decim(self, decim):
-        self.decim = decim
 
     def get_dac_rate(self):
         return self.dac_rate
@@ -213,12 +426,11 @@ class uhf_rfid_get_epc(gr.top_block, Qt.QWidget):
         self.ampl = ampl
         self.multiply_const_ff.set_k(self.ampl)
 
-    def get_adc_rate(self):
-        return self.adc_rate
+    def get_GUI_samples(self):
+        return self.GUI_samples
 
-    def set_adc_rate(self, adc_rate):
-        self.adc_rate = adc_rate
-        self.uhd_usrp_source.set_samp_rate(self.adc_rate)
+    def set_GUI_samples(self, GUI_samples):
+        self.GUI_samples = GUI_samples
 
 
 def main(top_block_cls=uhf_rfid_get_epc, options=None):
